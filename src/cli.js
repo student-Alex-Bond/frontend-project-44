18cf28b3df82e43e6cbb12bf6ea0c8c4134b21c7
Alex Bond <bondarenko.aleksandr.25@gmail.com>
2022-10-21 11:48:01 +0600

    added function hello

---
+++ src/cli.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync';
+
+export const hello = () => {
+    const userName = readlineSync.question('May I have your name? ');
+    console.log('Hi ' + userName + '!');
+}
