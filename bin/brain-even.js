e8deb62491ca175c909c27825530cf52a150baf9
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-11-15 19:43:36 +0600

    correct brain-even.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,5 @@
-import { hello } from '../src/cli.js'
-import readlineSync from 'readline-sync';
+#!/usr/bin/env node
 
-console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ');
-console.log('Hello, ' + userName + '!');
-console.log('Answer "yes" if the number is even, otherwise answer "no"')
+import playEven from '../src/games/even.js';
 
-const wrongAnswer = () => {
-    count = -10
-    console.log(`'yes' is wrong answer ;(. Correct answer was 'no'`)
-}
-
-
-
-let count = 3
-while (count !== 0 && count >= 0) {
-    const getRandomInt = (max) => {
-        return Math.floor(Math.random() * max);
-    }
-    const randomNumber = getRandomInt(100)
-    console.log('Question: ', randomNumber)
-    const answer = readlineSync.question('Your answer: ')
-
-    const isEven = (answer) => answer % 2 === 0 ? 'yes' : 'no'
-    isEven(randomNumber) === answer ? console.log('Correct') : wrongAnswer()
-    count -= 1
-}
-if (count === 0) {
-    console.log(`Congratulations, ${userName}`)
-}
+playEven();
