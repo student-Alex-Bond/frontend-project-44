d688992c2be46f6e304a206d74dece2044f6cd55
Alex_Bond <76467364+student-Alex-Bond@users.noreply.github.com>
2022-11-15 19:51:38 +0600

    fix eslint

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { hello } from '../src/cli.js'
+import hello from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-hello()
+hello();
--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const hello = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hello, ' + userName + '!');
-}
+const hello = () => {
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+};
+
+export default hello;
